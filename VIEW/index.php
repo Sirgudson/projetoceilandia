44dc76707f24f7dbd16c1454410a238101311b89
Marcelo Santos <marksantosdf@gmail.com>
2022-04-23 12:20:38 -0300

    commit teste
    Testando a página inicial

--- VIEW/index.php
+++ VIEW/index.php
@@ -1,5 +1,13 @@
-<?php
-
-echo "Hello World";
-
-?>
+<!DOCTYPE html>
+<html lang="PT_br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/style.css">
+    <title>Projeto Ceilândia</title>
+</head>
+<body>
+    <h1>TURISMO CANDANGO: PROJETO CEILÂNDIA</h1>
+</body>
+</html>
