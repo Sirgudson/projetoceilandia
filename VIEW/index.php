2f7c2a4a093caa7029624933f75248f30413807c
Marcelo Santos <marksantosdf@gmail.com>
2022-04-23 22:36:43 -0300

    Home no Desktop - Primeira dobra

--- VIEW/index.php
+++ VIEW/index.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html lang="PT_br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/CSS/style.css">
+    <link rel="stylesheet" href="/LIB/bootstrap/css/bootstrap.min.css">
     <title>Projeto Ceilândia</title>
 </head>
+
 <body>
-    <h1>TURISMO CANDANGO: PROJETO CEILÂNDIA</h1>
+    <div class="flex-container mt-4">
+        <div><a href="#">Início</a></div>
+        <div><a href="#">História</a></div>
+        <div><a href="#">Pontos Turísticos</a></div>
+        <div><a href="#">Contato</a></div>
+        <div><a href="#">Passeios</a></div>
+    </div>
+
+    <h1 class="titulo">Bem-vindo a Ceilândia</h1>
 </body>
+
 </html>
