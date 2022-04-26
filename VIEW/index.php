b09d414df4219c15bacd1d438e6ebb602b77e881
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-04-26 20:33:46 -0300

    Biblioteca de ícones e botão fixo do Whatsapp

--- VIEW/index.php
+++ VIEW/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/CSS/style.css">
     <link rel="stylesheet" href="/LIB/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     <title>Projeto Ceilândia</title>
 </head>
 
@@ -21,6 +22,25 @@
             <img src="/ASSETS/SVG/menu.svg" alt="menu">
         </div>
     </div>
+    
+        <!-- Botão Whatsapp-->
+    <a href="https://wa.me/" style="
+         position: fixed;
+         width: 60px;
+         height: 60px;
+         bottom: 40px;
+         right: 30px;
+         background-color: #26A999;
+         color: #fff;
+         border-radius: 50px;
+         text-align: center;
+         font-size: 30px;
+         box-shadow: 1px 1px 2px #888;
+         z-index: 1000;
+         " target="_blank">
+   <i style="margin-top: 16px" class="fa fa-whatsapp"></i>
+  </a>
+
     <div class="apresentacao">
         <h2 class="subtitulo">
             <img src="/ASSETS/IMAGES/onda1.png" alt="onda1">BE MY GUEST<img src="/ASSETS/IMAGES/onda2.png" alt="onda2">
