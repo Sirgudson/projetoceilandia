d15e2be1e4a9c4dacc2096e127d1674b2985b1bd
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-04-27 20:46:24 -0300

    Criando footer + ajustes finais da home page

--- VIEW/index.php
+++ VIEW/index.php
@@ -104,12 +104,35 @@
                 </p>
             </div>
         </div>
-        <div class="item7">7</div>
-        <div class="item8">8</div>
-        <div class="item9">9</div>
     </div>
 
     <!-- FOOTER -->
+    <footer class="box-footer">
+        <div class="flex-container2">
+            <div class="logo redes">
+                <img src="/ASSETS/SVG/logo1.svg" alt="">
+            </div>
+            <div class="redes">
+                <p>Siga-nos</p>
+                <img src="/ASSETS/IMAGES/separador-laranja.png" alt="separador laranja" class="separador6">
+                
+                <div class="img-footer">
+                    <img class="ico-svg" src="/ASSETS/SVG/facebook.svg" alt="icone facebook">
+                    <img class="ico-svg" src="/ASSETS/SVG/instagram.svg" alt="icone instagram">
+                    <img class="ico-svg" src="/ASSETS/SVG/twitter.svg" alt="icone twitter">
+                </div>
+            </div>
+            <div class="novidade redes">
+                <p>Receba nossas novidades</p>
+                <img src="/ASSETS/IMAGES/separador-laranja.png" alt="separador laranja" class="separador6">
+                <div>
+                <input class="input-text" type="text" value="Insira seu e-mail aqui*"><br>
+                <input class="input-submit" type="submit" value="Participar">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <p class="fim">2022 ©Turismo Candando: Projeto Ceilândia</p>
 </body>
 
 </html>
