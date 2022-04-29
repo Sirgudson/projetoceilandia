f701491b4d8e34a58db4242873bedbcb089658ef
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-04-28 21:12:39 -0300

    Ajustes responsivo

---
+++ VIEW/footer.php
@@ -0,0 +1,26 @@
+<!-- FOOTER -->
+<footer class="box-footer">
+        <div class="flex-container2">
+            <div class="logo redes">
+                <img src="../ASSETS/SVG/logo1.svg" alt="">
+            </div>
+            <div class="redes">
+                <p>Siga-nos</p>
+                <img src="../ASSETS/IMAGES/separador-laranja.png" alt="separador laranja" class="separador6">
+                
+                <div class="img-footer">
+                    <img class="ico-svg" src="../ASSETS/SVG/facebook.svg" alt="icone facebook">
+                    <img class="ico-svg" src="../ASSETS/SVG/instagram.svg" alt="icone instagram">
+                    <img class="ico-svg" src="../ASSETS/SVG/twitter.svg" alt="icone twitter">
+                </div>
+            </div>
+            <div class="novidade redes">
+                <p>Receba nossas novidades</p>
+                <img src="../ASSETS/IMAGES/separador-laranja.png" alt="separador laranja" class="separador6">
+                <div>
+                <input class="input-text" type="text" value="Insira seu e-mail aqui*"><br>
+                <input class="input-submit" type="submit" value="Participar">
+                </div>
+            </div>
+        </div>
+    </footer>
