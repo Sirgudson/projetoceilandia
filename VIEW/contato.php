f673f8b7d8ce0eea840a5fa3ca7c2dc3a54c9fb8
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-10 16:44:46 -0300

    Adicionando favicon nas paginas

--- VIEW/contato.php
+++ VIEW/contato.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
     <link rel="stylesheet" href="../LIB/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     <title>Projeto Ceilândia</title>
