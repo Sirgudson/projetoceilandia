4d1fc99733f0de0d5a35ed577096dabf7d2c13e3
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-10 16:39:33 -0300

    Estruturando novas paginas

--- VIEW/passeio.php
+++ VIEW/passeio.php
@@ -1,5 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="PT_br">
 
-//silence is goden
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../LIB/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+    <title>Projeto Ceilândia</title>
+</head>
 
-?>
+<body>
+    <!-- HEADER -->
+    <?php include('header.php') ?>
+
+    <div class="apresentacao">
+        <h1 class="titulo">Passeio</h1>
+        <img src="../ASSETS/IMAGES/onda1.png" alt="onda1">
+    </div>
+
+
+    <!-- FOOTER -->
+    <?php include('footer.php') ?>
+</body>
+
+</html>
