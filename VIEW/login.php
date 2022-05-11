8cd0906fd83e703f8a1cde33f76801f1659777f1
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-10 22:15:28 -0300

    FrontEnd page login

--- VIEW/login.php
+++ VIEW/login.php
@@ -1,36 +1,76 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/style-internas.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title>Projeto Ceilândia</title>
 </head>
-
 <body>
-
-    <form action="/action_page.php" method="post">
-        <div class="loginImage">
-            <img src="../ASSETS/SVG/logo1.svg" alt="logo">
+    <div class="container">
+        <div class="content first-content">
+            <div class="first-column">
+                <h2 class="title title-primary">bem-vindo novamente!</h2>
+                <p class="description description-primary">Para se conectar faça seu login</p>
+                <p class="description description-primary">e continue sua experiência em nosso site</p>
+                <button id="signin" class="btn btn-primary">entrar</button>
+            </div> 
+               
+            <div class="second-column">
+                <h2 class="title title-second">crie sua conta</h2>
+                <form class="form">
+                    <label class="label-input" for="">
+                        <i class="far fa-user icon-modify"></i>
+                        <input type="text" placeholder="Nome">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" placeholder="E-mail">
+                    </label>
+                    
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Senha">
+                    </label>
+                    
+                    
+                    <button class="btn btn-second">criar</button>        
+                </form>
+            </div>
         </div>
 
-        <div class="container">
-            <label for="uname"><b>Email</b></label>
-            <input type="text" placeholder="Digite seu email" name="email" required>
+        <div class="content second-content">
+            <div class="first-column">
+                <h2 class="title title-primary">olá, visitante!</h2>
+                <p class="description description-primary">cadastre-se e</p>
+                <p class="description description-primary">venha agendar seu passeio</p>
+                <button id="signup" class="btn btn-primary">criar</button>
+            </div>
+            <div class="second-column">
+                <h2 class="title title-second">faça seu login</h2>
 
-            <label for="psw"><b>Senha</b></label>
-            <input type="password" placeholder="Digite sua senha" name="psw" required>
-
-            <button type="submit">Login</button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember"> Lembrar login
-            </label>
+                <form class="form">
+                
+                    <label class="label-input" for="">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" placeholder="E-mail">
+                    </label>
+                
+                    <label class="label-input" for="">
+                        <i class="fas fa-lock icon-modify"></i>
+                        <input type="password" placeholder="Senha">
+                    </label>
+                
+                    <a class="password" href="../VIEW/resetpassword.php">esqueceu sua senha?</a>
+                    <button class="btn btn-second">entrar</button>
+                </form>
+            </div>
         </div>
-
-        <div class="container" style="background-color:#f1f1f1">
-            <a href="../VIEW/index.php" class="cancelbtn">Cancelar</a>
-            <span class="psw">Esqueceu a <a href="../VIEW/resetpassword.php">senha?</a></span>
-        </div>
-    </form>
-
+    </div>
+    <script src="../JS/function2.js"></script>
 </body>
-
 </html>
