5f7b753a9a24297c08c801fe898a5437767a54ee
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-13 19:56:14 -0300

    Ajuste page historia

--- VIEW/historia.php
+++ VIEW/historia.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
     <link rel="stylesheet" href="../LIB/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     <title>Projeto Ceilândia</title>
@@ -51,8 +52,42 @@
             <!--<div class="texto-compare">Caixa D'agua contruída em 1971, é tombada como patrimonio histórico do DF.</div>-->
         </div>
         <div class="box-history3">
-        <h3 class="text-cultura">Prosopopéia contada pela moradora Nadir Sousa</h3>
+            <h3 class="text-cultura1">Prosopopéia de Ceilândia</h3>
             <img src="../ASSETS/IMAGES/separador-laranja.png" alt="separador laranja" class="separador-history2">
+            <p class="texto-box-history2">
+                Bem-vindo! </p>
+
+            <p class="texto-box-history2">
+                Sou Ceilândia, a morena nordestina do centro Oeste. Tenho muita história para te contar.
+            </p>
+            <p class="texto-box-history2">
+                Eu nasci da esperança de um povo remanescente dos interiores do nosso amado Brasil. Sou parte desse povo retumbante, que até hoje tem o grito em sua voz... São vozes que cantam suas dores, seus amores e suas lutas, também clamam por justiça, paz e misericórdia.
+            </p>
+            <p class="texto-box-history2">
+                Eu nasci do enjeito dos fortes para amparar os fracos, e fui me fortalecendo, crescendo para alimentar meu povo, como minhas filhas fizeram e fazem. É verdade que o enjeito ainda permanece sob várias nuances, mas as experiências e a força que nos são características ajudam-nos a seguir, vislumbrar horizontes que dantes não ousaríamos "enxergar".
+            </p>
+            <p class="texto-box-history2">
+                Tive nos feirantes a maior expressão de fidelidade, e o comércio foi se fortalecendo, e vi nascer artistas de rua com suas sanfonas e suas danças marcantes, criei atletas, capoeiristas, juízes, grandes empresários,8 musicistas das mais variadas expressões e acreditaram em mim.
+            </p>
+            <p class="texto-box-history2">
+                Recebi com felicidades os bancos que começavam a crer no meu povo e o comércio cresceu e se fortaleceu... Ganhei aglomerados de prédios com várias lojas e foi chamado de centro da Ceilândia. Ganhei vida noturna e meu povo poder ter vida noturna também... Fui me expandindo e me valorizando; ganhei asfalto na via norte e meu povo caminhava muito tempo para ter acesso ao transporte público, a água? Como era difícil dessedentar meu povo e quantas dificuldades vivemos...
+            </p>
+            <p class="texto-box-history2">
+                Fui ficando madura, aumentando minha família e ganhando defensores como Maria de Lourdes Abadia que cuidou de mim de 1975 até 1985 e fui ganhando bairros, e trilhas de asfalto que me dividem e organizam meu povo. E me tornei a maior Região Administrativa do Distrito Federal.
+            </p>
+            <p class="texto-box-history2">
+                Também sofri por ver os filhos dos meus filhos se perderem para as drogas por falta de lazer e de empenho público.
+            </p>
+            <p class="texto-box-history2">
+                Hoje sou muito feliz e os meus filhos e netos me embelezam com seus bens e beleza. Há neles muita exuberância, alegria, música, festas, mas também há dores e lágrimas como existe em toda boa família Nordestina.
+            </p>
+            <p class="texto-box-history2">Pode entrar.</p>
+            <p class="texto-box-history2">Nadir Ferreira de Sousa </p>
+            <p class="texto-box-history2">A partir das experiências vividas <br>
+                & <br>
+                Apaixonada por esta Cidade <br>
+                <br>
+            </p>
         </div>
     </div>
 
@@ -60,4 +95,5 @@
     <?php include('footer.php') ?>
     <script src="../JS/function.js"></script>
 </body>
+
 </html>
