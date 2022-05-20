d9b84f69009d901675a52a4ef5b69b5102939e86
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-20 20:42:54 -0300

    Adicionando page admin (incompleta)

---
+++ VIEW/admin.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="PT_br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../CSS/style-admin.css">
+  <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+  <link rel="stylesheet" href="../LIB/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <title>Projeto Ceilândia</title>
+</head>
+
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class="fa fa-solid fa-water"></i>
+      <span class="logo-name">Projeto Ceilândia</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+          <a href="#">
+            <i class="fa fa-solid fa-water"></i>
+            <span class="link_name">Menu</span>
+          </a>
+      </li>
+      <li>
+        <a href="#">
+          <!--<i class="fa-solid fa-water"></i>-->
+          <span class="link_name">Pontos Turísticos</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <!--<i class="fa-solid fa-water"></i>-->
+          <span class="link_name">Passeio</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fa fa-solid fa-users"></i>
+          <span class="link_name">Usuários</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <!--<i class="fa-solid fa-water"></i>-->
+          <span class="link_name">Avaliações</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</body>
+
+</html>
+
+<!--   <main class="main">
+    <aside class="sidebar">
+      <nav class="nav">
+        <ul>
+          <li class="active"><a href="#">Listar Usuários</a></li>
+          <li><a href="#">Alterar Usuários</a></li>
+          <li><a href="#">Listar Pontos Turísticos</a></li>
+          <li><a href="#">Alterar Pontos Turísticos</a></li>
+          <li><a href="#"> Eventos</a></li>
+        </ul>
+      </nav>
+    </aside>
+
+    <section class="twitter">
+      <div class="container">
+        <!--<a target="_blank" href="https://twitter.com/ReisnerShawn">
+          <img class="social" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/twitter-128.png">
+        </a>
+        <p>Follow me</p>
+        <p>on Twitter!</p>-->
+<!-- </div>
+    </section>
+  </main>
