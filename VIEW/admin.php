5fb3ee4b2fa87843a19a5fa59c42cd57c4dc9989
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-24 22:35:01 -0300

    Novos ajustes da page admin

--- VIEW/admin.php
+++ VIEW/admin.php
@@ -15,116 +15,130 @@
 </head>
 
 <body>
-
-  <body>
-    <div class="sidebar close">
-      <div class="logo-details">
-        <img src="../ASSETS/ICO/logo-branca.ico" alt="">
-        <span class="logo_name">Projeto Ceilândia</span>
-      </div>
-      <ul class="nav-links">
-        <li>
-          <a href="#">
-          <i class="fa fa-regular fa-toolbox"></i>
-            <span class="link_name">Menu Admin</span>
-          </a>
-          <ul class="sub-menu blank">
-            <li><a class="link_name" href="#">Menu Admin</a></li>
-          </ul>
-        </li>
-        <li>
-          <div class="iocn-link">
-            <a href="#">
-              <i class="fa fa-regular fa-users"></i>
-              <span class="link_name">Usuários</span>
-            </a>
-            <i class='bx bxs-chevron-down arrow'></i>
-          </div>
-          <ul class="sub-menu">
-            <li><a class="link_name" href="#">Usuários</a></li>
-            <a onclick="iframeRedirect('http://localhost/projetoceilandia/VIEW/listarUsuario.php')" id="a"></a>
-            <li><a href="#">Todos</a></li>
-            <li><a href="#">Cadastrar</a></li>
-          </ul>
-        </li>
-        <li>
-          <div class="iocn-link">
-            <a href="#">
-            <i class="fa fa-regular fa-paragraph"></i>
-              <span class="link_name">Pontos Turísticos</span>
-            </a>
-            <i class='bx bxs-chevron-down arrow'></i>
-          </div>
-          <ul class="sub-menu">
-            <li><a class="link_name" href="#">Pontos Turísticos</a></li>
-            <li><a href="#">Todos</a></li>
-            <li><a href="#">Cadastrar</a></li>
-          </ul>
-        </li>
-        <li>
-          <div class="iocn-link">
-            <a href="#">
-            <i class="fa fa-regular fa-business-time"></i>
-              <span class="link_name">Passeios</span>
-            </a>
-            <i class='bx bxs-chevron-down arrow'></i>
-          </div>
-          <ul class="sub-menu">
-            <li><a class="link_name" href="#">Passeios</a></li>
-            <li><a href="#">Todos</a></li>
-            <li><a href="#">Cadastrar</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">
-          <i class="fa fa-regular fa-star"></i>
-            <span class="link_name">Avaliações</span>
-          </a>
-          <ul class="sub-menu blank">
-            <li><a class="link_name" href="#">Avaliações</a></li>
-          </ul>
-        </li>
-        <li>
-          <div class="profile-details">
-            <div class="profile-content">
-              <!--<img src="image/profile.jpg" alt="profileImg">-->
-            </div>
-            <div class="name-job">
-              <div class="profile_name">Sair?</div>
-              <div class="job">Projeto Ceilândia</div>
-            </div>
-            <a href="../VIEW/index.php"><i class='bx bx-log-out' id="log-aut" onclick="log_out()"></i></a>
-          </div>
-        </li>
-      </ul>
+  <div class="sidebar close">
+    <div class="logo-details">
+      <img src="../ASSETS/ICO/logo-branca.ico" alt="">
+      <span class="logo_name">Projeto Ceilândia</span>
     </div>
-    <section class="home-section">
-      <div class="home-content">
-        <i class='bx bx-menu'></i>
-        <span class="text">Turismo Candango: PROJETO CEILÂNDIA</span>
-      </div>
-    </section>
-    <iframe src="" frameborder="0" class="iframe"></iframe>
-    <script>
-      let arrow = document.querySelectorAll(".arrow");
-      for (var i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener("click", (e) => {
-          let arrowParent = e.target.parentElement.parentElement; //selecting main parent of arrow
-          arrowParent.classList.toggle("showMenu");
-        });
-      }
-      let sidebar = document.querySelector(".sidebar");
-      let sidebarBtn = document.querySelector(".bx-menu");
-      console.log(sidebarBtn);
-      sidebarBtn.addEventListener("click", () => {
-        sidebar.classList.toggle("close");
-      });
 
-      function log_out(){
-            return confirm("Tem certeza que desejar sair?");
-        }      
-    </script>
-  </body>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class="fa fa-regular fa-toolbox"></i>
+          <span class="link_name">Menu Admin</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Menu Admin</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="fa fa-regular fa-users"></i>
+            <span class="link_name">Usuários</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Usuários</a></li>
+          <a href="#"></a>
+          <li><a href="#" onclick="carregarIframe('listarUsuario.php')">Todos</a></li>
+          <li><a href="#">Cadastrar</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="fa fa-regular fa-paragraph"></i>
+            <span class="link_name">Pontos Turísticos</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Pontos Turísticos</a></li>
+          <li><a href="#">Todos</a></li>
+          <li><a href="#">Cadastrar</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="fa fa-regular fa-business-time"></i>
+            <span class="link_name">Passeios</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Passeios</a></li>
+          <li><a href="#">Todos</a></li>
+          <li><a href="#">Cadastrar</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">
+          <i class="fa fa-regular fa-star"></i>
+          <span class="link_name">Avaliações</span>
+        </a>
+
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Avaliações</a></li>
+        </ul>
+      </li>
+
+      <li>
+        <div class="profile-details">
+          <div class="profile-content">
+            <!--<img src="../ASSETS/IMAGES/equipe.jpeg">-->
+          </div>
+          <div class="name-job">
+            <div class="profile_name">Sair?</div>
+            <div class="job">Projeto Ceilândia</div>
+          </div>
+          <a href="../VIEW/index.php"><i class='bx bx-log-out' id="log-aut" onclick="log_out()"></i></a>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu'></i>
+      <span class="text">Turismo Candango: PROJETO CEILÂNDIA</span>
+    </div>
+  </section>
+  <iframe src="" frameborder="0"  id="iframe"></iframe>
+  <script>
+    let arrow = document.querySelectorAll(".arrow");
+    for (var i = 0; i < arrow.length; i++) {
+      arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("showMenu");
+      });
+    }
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarBtn = document.querySelector(".bx-menu");
+    console.log(sidebarBtn);
+    sidebarBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("close");
+    });
+
+    function log_out() {
+      return confirm("Tem certeza que desejar sair?");
+    }
+
+    function carregarIframe(src) {
+      const iframe = document.querySelector("#iframe");
+      iframe.src = src;
+    }
+  </script>
+</body>
 
 </html>
 
--- VIEW/listarUsuario.php
+++ VIEW/listarUsuario.php
@@ -4,17 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Lista de Usuários</title>
     <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/style-admin.css">
     <script src="../LIB/fontawesome-free-6.1.1-web/js/all.min.js"></script>
-    <style>
-       tr:nth-child(even) {
-          background-color:#cccccc;
-       }
-       tr:nth-child(odd) {
-           background-color:#ffff;
-       }
-    </style>
 </head>
 
 <body>
@@ -23,13 +17,15 @@ require_once '../DAO/ClienteDAO.php';
 include '../JS/funcao.php';
 $clienteDAO = new ClienteDAO();
 $usuarios = $clienteDAO->findAll();
-echo "<table border='1'>";
+echo "<div class='container-list'>";
+echo "<h1><b>Usuários</b></h1>";
+echo "<table>";
 echo "  <tr>";
 echo "      <th>Nome</th>";
 echo "      <th>CPF</th>";
 echo "      <th>Email</th>";
-echo "      <th>Excluir</th>";
 echo "      <th>Editar</th>";
+echo "      <th>Excluir</th>";
 echo "  </tr>";
 
 foreach ( $usuarios as $usuario ) {
@@ -37,11 +33,12 @@ foreach ( $usuarios as $usuario ) {
     echo "  <td> {$usuario["NOME"]}</td>";
     echo "  <td>", formatarCpfCnpj( $usuario["CPF"] ), "</td>";
     echo "  <td> {$usuario["EMAIL"]} </td>";
-    echo "  <td align='center'><a href='../controller/excluirUsuarioController.php?id={$usuario["ID"]}'><i class='fa-solid fa-trash-can'></a></i></td>";
-    echo "  <td align='center'><a href='alterarUsuario.php?id={$usuario["ID"]}'><i class='fa-solid fa-pen-to-square'></a></i></td>";
+    echo "  <td align='center'><a href='alterarUsuario.php?id={$usuario["ID"]}'><i class='icon fa-solid fa-user-pen'></a></i></td>";
+    echo "  <td align='center'><a href='../controller/excluirUsuarioController.php?id={$usuario["ID"]}'><i class='delete fa-solid fa-user-xmark'></a></i></td>";
     echo "</tr>";
 }
 echo "</table>";
+echo "</div>";
 ?>
 </body>
 </html>
