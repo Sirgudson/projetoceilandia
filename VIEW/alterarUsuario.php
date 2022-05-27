7ea600b31816c146b9c7cc1febe7804234a30188
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-26 22:54:38 -0300

    Alterando title da page alterar usuário

--- VIEW/alterarUsuario.php
+++ VIEW/alterarUsuario.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Usuários</title>
+    <title>Alterar Usuários</title>
     <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
     <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
     <link rel="stylesheet" href="../CSS/style-admin.css">
