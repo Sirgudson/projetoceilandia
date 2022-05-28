82214f0fd919e961a33744ba245ce4539fda9f03
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-05-28 00:01:49 -0300

    CSS para listar Ponto e Passeio

--- VIEW/listarEvento.php
+++ VIEW/listarEvento.php
@@ -1,53 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Lista de Eventos</title>
     <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/style-admin.css">
     <script src="../LIB/fontawesome-free-6.1.1-web/js/all.min.js"></script>
-    <style>
-       tr:nth-child(even) {
-          background-color:#cccccc;
-       }
-       tr:nth-child(odd) {
-           background-color:#ffff;
-       }
-    </style>
 </head>
 
 <body>
-<?php
-require_once '../DAO/EventoDAO.php';
-include '../JS/funcao.php';
-$eventoDAO = new EventoDAO();
-$eventos = $eventoDAO->findAll();
-echo "<table border='1'>";
-echo "  <tr>";
-echo "      <th>Localização</th>";
-echo "      <th>Título</th>";
-echo "      <th>Descrição</th>";
-echo "      <th>Início</th>";
-echo "      <th>Encerramento</th>";
-echo "      <th>Capacidade</th>";
-echo "      <th>Excluir</th>";
-echo "      <th>Editar</th>";
-echo "  </tr>";
+    <?php
+    require_once '../DAO/EventoDAO.php';
+    include '../JS/funcao.php';
+    $eventoDAO = new EventoDAO();
+    $eventos = $eventoDAO->findAll();
+    echo "<div class='container-table'>";
+    echo "<h1><b>Eventos</b></h1>";
+    echo "<table>";
+    echo "  <tr>";
+    echo "      <th>Localização</th>";
+    echo "      <th>Título</th>";
+    echo "      <th>Descrição</th>";
+    echo "      <th>Início</th>";
+    echo "      <th>Encerramento</th>";
+    echo "      <th>Capacidade</th>";
+    echo "      <th>Editar</th>";
+    echo "      <th>Excluir</th>";
+    echo "  </tr>";
 
-foreach ( $eventos as $evento ) {
-    echo "<tr>";
-    echo "  <td> {$evento["LOCAL"]}</td>";
-    echo "  <td> {$evento["TITULO"]}</td>";
-    echo "  <td> {$evento["DESCRICAO"]} </td>";
-    echo "  <td> {$evento["DATA_INICIO"]} </td>";
-    echo "  <td> {$evento["DATA_TERMINO"]} </td>";
-    echo "  <td> {$evento["CAPACIDADE"]} </td>";
-    echo "  <td align='center'><a href='../controller/excluirEventoController.php?id={$evento["ID"]}'><i class='fa-solid fa-trash-can'></a></i></td>";
-    echo "  <td align='center'><a href='alterarEvento.php?id={$evento["ID"]}'><i class='fa-solid fa-pen-to-square'></a></i></td>";
-    echo "</tr>";
-}
-echo "</table>";
-?>
+    foreach ($eventos as $evento) {
+        echo "<tr>";
+        echo "  <td> {$evento["LOCAL"]}</td>";
+        echo "  <td> {$evento["TITULO"]}</td>";
+        echo "  <td> {$evento["DESCRICAO"]} </td>";
+        echo "  <td> {$evento["DATA_INICIO"]} </td>";
+        echo "  <td> {$evento["DATA_TERMINO"]} </td>";
+        echo "  <td> {$evento["CAPACIDADE"]} </td>";
+        echo "  <td align='center'><a href='alterarEvento.php?id={$evento["ID"]}'><i class='icon fa-solid fa-pen-to-square'></a></i></td>";
+        echo "  <td align='center'><a href='../controller/excluirEventoController.php?id={$evento["ID"]}'><i class='delete fa-solid fa-trash-can'></a></i></td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "</div>";
+    ?>
 </body>
+
 </html>
--- VIEW/listarPonto.php
+++ VIEW/listarPonto.php
@@ -2,18 +2,17 @@
 <html lang="PT_br">
 
 <head>
-<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Pontos Turísticos</title>
-    <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
-    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
-    <link rel="stylesheet" href="../CSS/style-admin.css">
-    <script src="../LIB/fontawesome-free-6.1.1-web/js/all.min.js"></script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lista de Pontos Turísticos</title>
+  <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
+  <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+  <link rel="stylesheet" href="../CSS/style-admin.css">
+  <script src="../LIB/fontawesome-free-6.1.1-web/js/all.min.js"></script>
 </head>
 
 <body>
-
   <?php
   require_once '../DAO/PontoDAO.php';
   include '../JS/funcao.php';
@@ -26,8 +25,8 @@
   echo "      <th>Localização</th>";
   echo "      <th>Título</th>";
   echo "      <th>Descrição</th>";
-  echo "      <th>Excluir</th>";
   echo "      <th>Editar</th>";
+  echo "      <th>Excluir</th>";
   echo "  </tr>";
 
   foreach ($pontos as $ponto) {
