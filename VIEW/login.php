687e091d2885b3e890379bd77ea74c836a8a2ca9
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-06-02 21:39:24 -0300

    Modal para resetar senha

--- VIEW/login.php
+++ VIEW/login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../CSS/style-login.css">
     <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
@@ -11,6 +12,7 @@
     <script src="../js/jquery-validation-1.19.3/dist/jquery.validate.min.js"></script>
     <title>Projeto Ceilândia</title>
 </head>
+
 <body>
     <div class="container">
         <div class="content first-content">
@@ -19,8 +21,8 @@
                 <p class="description description-primary">Para se conectar faça seu login</p>
                 <p class="description description-primary">e continue sua experiência em nosso site</p>
                 <button id="signin" class="btn btn-primary">entrar</button>
-            </div> 
-               
+            </div>
+
             <div class="second-column">
                 <h2 class="title title-second">crie sua conta</h2>
                 <form id="formCadastrarUsuario" action="../controller/cadastrarUsuarioController.php" class="form" method="post">
@@ -33,19 +35,19 @@
                         <i class="far fa-user icon-modify"></i>
                         <input type="text" name="cpf" id="cpf" placeholder="CPF">
                     </label>
-            
+
                     <label class="label-input" for="">
                         <i class="far fa-envelope icon-modify"></i>
                         <input type="email" name="email" id="email" placeholder="E-mail">
                     </label>
-                    
+
                     <label class="label-input" for="">
                         <i class="fas fa-lock icon-modify"></i>
                         <input type="password" name="senha" id="senha" placeholder="Senha">
                     </label>
                     <input type="hidden" name="ativo" id="ativo" value="1">
-                    
-                    <button class="btn btn-second" type="submit">criar</button>        
+
+                    <button class="btn btn-second" type="submit">criar</button>
                 </form>
             </div>
         </div>
@@ -65,33 +67,81 @@
                         <i class="far fa-envelope icon-modify"></i>
                         <input type="email" name="email" placeholder="E-mail" required>
                     </label>
-                
+
                     <label class="label-input" for="">
                         <i class="fas fa-lock icon-modify"></i>
                         <input type="password" name="senha" placeholder="Senha" required>
                     </label>
-                
-                    <a class="password" href="../VIEW/resetpassword.php">esqueceu sua senha?</a>
+
+                    <a class="password" href="#" id="mySizeChart">esqueceu sua senha?</a>
                     <button class="btn btn-second" type="submit">entrar</button>
                 </form>
             </div>
         </div>
     </div>
+    <div id="mySizeChartModal" class="ebcf_modal">
+
+        <div class="ebcf_modal-content">
+            <span class="ebcf_close">&times;</span>
+            <h3>Reset sua senha!</h3>
+            <form action="/action_page.php" method="post">
+                <div class="container-reset">
+                    <label class="label label-input" for="uname">
+                        <i class="far fa-envelope icon-modify"></i>
+                        <input type="email" name="email" placeholder="E-mail" required>
+                    </label>
+                </div>
+
+                <div class="container-reset2">
+                    <input type="submit" value="Enviar" id="enviar">
+                    <a href="../VIEW/login.php" class="cancelbtn">Cancelar</a>
+                </div>
+            </form>
+        </div>
+
+    </div>
     <script src="../JS/function2.js"></script>
+    <script>
+        // Get the modal
+        var ebModal = document.getElementById('mySizeChartModal');
+
+        // Get the button that opens the modal
+        var ebBtn = document.getElementById("mySizeChart");
+
+        // Get the <span> element that closes the modal
+        var ebSpan = document.getElementsByClassName("ebcf_close")[0];
+
+        // When the user clicks the button, open the modal 
+        ebBtn.onclick = function() {
+            ebModal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        ebSpan.onclick = function() {
+            ebModal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == ebModal) {
+                ebModal.style.display = "none";
+            }
+        }
+    </script>
     <div style="text-align: center;">
-<?php
-    if ( isset( $_GET["msg"] ) ) {
-        echo $_GET["msg"];
-    }
-?>
+        <?php
+        if (isset($_GET["msg"])) {
+            echo $_GET["msg"];
+        }
+        ?>
     </div>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
             $('#cpf').mask('000.000.000-00');
         });
     </script>
     <script>
-        $( "#formCadastrarUsuario" ).validate({
+        $("#formCadastrarUsuario").validate({
             rules: {
                 nome: {
                     required: true,
@@ -114,4 +164,5 @@
         });
     </script>
 </body>
+
 </html>
