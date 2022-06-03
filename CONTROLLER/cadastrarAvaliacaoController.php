b272451d1671dab2ca89fc3022bdeea53eba3967
Marcelo Santos <marcelo.santos@poder360.com.br>
2022-06-03 20:22:22 -0300

    Corrigindo bootstrap dos alerts

--- CONTROLLER/cadastrarAvaliacaoController.php
+++ CONTROLLER/cadastrarAvaliacaoController.php
@@ -1,3 +1,6 @@
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
 <?php
     require_once '../DTO/AvaliacaoDTO.php';
     require_once '../DAO/AvaliacaoDAO.php';
@@ -17,7 +20,7 @@
     if ( !empty( $avaliacao ) ) {
         if ( $avaliacaoDAO->salvar( $avaliacaoDTO ) ) {
         ?>
-            <div class="alert alert-danger" role="alert">
+            <div class="alert alert-success" role="alert">
                 <p>Deu certo!</p>
                 Vamos te redirecionar para a página Inicial..
                 <?php header( 'Refresh: 4; URL=http://localhost/projetoceilandia/VIEW/index.php' );?>
