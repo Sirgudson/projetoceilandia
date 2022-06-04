9b07db7b75dc2ae83772fe9e016bcfb004b0b0f2
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-06-03 21:16:22 -0300

    Adicionando css do listar avaliações

--- VIEW/listarAvaliacao.php
+++ VIEW/listarAvaliacao.php
@@ -4,17 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Listar Avaliações</title>
     <link rel="stylesheet" href="../LIB/fontawesome-free-6.1.1-web/css/all.min.css">
+    <link rel="shortcut icon" href="../ASSETS/ICO/logo1.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/style-admin.css">
     <script src="../LIB/fontawesome-free-6.1.1-web/js/all.min.js"></script>
-    <style>
-       tr:nth-child(even) {
-          background-color:#cccccc;
-       }
-       tr:nth-child(odd) {
-           background-color:#ffff;
-       }
-    </style>
 </head>
 
 <body>
@@ -23,13 +17,15 @@ require_once '../DAO/AvaliacaoDAO.php';
 include '../JS/funcao.php';
 $avaliacaoDAO = new AvaliacaoDAO();
 $avaliacoes = $avaliacaoDAO->findAll();
-echo "<table border='1'>";
+echo "<div class='container-list'>";
+echo "<h1><b>Avaliações</b></h1>";
+echo "<table>";
 echo "  <tr>";
 echo "      <th>Nome</th>";
 echo "      <th>Opinião</th>";
 echo "      <th>Avaliação</th>";
-echo "      <th>Excluir</th>";
 echo "      <th>Editar</th>";
+echo "      <th>Excluir</th>";
 echo "  </tr>";
 
 foreach ( $avaliacoes as $avaliacao ) {
@@ -37,8 +33,8 @@ foreach ( $avaliacoes as $avaliacao ) {
     echo "  <td> {$avaliacao["NOME"]}</td>";
     echo "  <td> {$avaliacao["OPINIAO"]}</td>";
     echo "  <td> {$avaliacao["AVALIACAO"]} </td>";
-    echo "  <td align='center'><a href='../controller/excluirAvaliacaoController.php?id={$avaliacao["ID"]}'><i class='fa-solid fa-trash-can'></a></i></td>";
-    echo "  <td align='center'><a href='alterarAvaliacao.php?id={$avaliacao["ID"]}'><i class='fa-solid fa-pen-to-square'></a></i></td>";
+    echo "  <td align='center'><a href='alterarAvaliacao.php?id={$avaliacao["ID"]}'><i class='icon fa-solid fa-pen-to-square'></a></i></td>";
+    echo "  <td align='center'><a href='../controller/excluirAvaliacaoController.php?id={$avaliacao["ID"]}'><i class='delete fa-solid fa-trash-can'></a></i></td>";
     echo "</tr>";
 }
 echo "</table>";
