3631fc9366ecee6101cdd6dccf3e4186c7892bc1
Dhimylee Almeida <dhimylee.almeida@gmail.com>
2022-06-03 21:07:36 -0300

    Finalizando css da page avaliação

--- VIEW/avaliacao.php
+++ VIEW/avaliacao.php
@@ -54,23 +54,38 @@
             <div class="list_avaliacao">
                 <div class="rating__item">
                     <input class="avaliacao_input rating--1" id="rating-1-2" type="radio" value="1" name="avaliacao" />
-                    <label class="avaliacao_label rating--1" for="rating-1-2"><i class="fa fa-regular fa-star"></i></label>
+                    <label class="avaliacao_label rating--1" for="rating-1-2">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </label>
                 </div>
                 <div class="rating__item">
                     <input class="avaliacao_input rating--2" id="rating-2-2" type="radio" value="2" name="avaliacao" />
-                    <label class="avaliacao_label rating--2" for="rating-2-2"><i class="fa-regular fa-star"></i></label>
+                    <label class="avaliacao_label rating--2" for="rating-2-2">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </label>
                 </div>
                 <div class="rating__item">
                     <input class="avaliacao_input rating--3" id="rating-3-2" type="radio" value="3" name="avaliacao" />
-                    <label class="avaliacao_label rating--3" for="rating-3-2"><i class="fa-regular fa-star"></i></label>
+                    <label class="avaliacao_label rating--3" for="rating-3-2">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </label>
                 </div>
                 <div class="rating__item">
                     <input class="avaliacao_input rating--4" id="rating-4-2" type="radio" value="4" name="avaliacao" />
-                    <label class="avaliacao_label rating--4" for="rating-4-2"><i class="fa-regular fa-star"></i></label>
+                    <label class="avaliacao_label rating--4" for="rating-4-2">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </label>
                 </div>
                 <div class="rating__item">
                     <input class="avaliacao_input rating--5" id="rating-5-2" type="radio" value="5" name="avaliacao" />
-                    <label class="avaliacao_label rating--5" for="rating-5-2"><i class="fa-regular fa-star"></i></i></label>
+                    <label class="avaliacao_label rating--5" for="rating-5-2">
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </label>
                 </div>
 
 
@@ -82,6 +97,32 @@
             <input type="submit" value="Cadastrar">
         </div>
     </form>
+    <div class="listar-avaliacao">
+        <h3>Veja outra avaliações</h3>
+        <?php
+        require_once '../DAO/AvaliacaoDAO.php';
+        include '../JS/funcao.php';
+        $avaliacaoDAO = new AvaliacaoDAO();
+        $avaliacoes = $avaliacaoDAO->findAll();
+
+        foreach ($avaliacoes as $avaliacao) {
+            echo "<div class='box-listar'";
+            echo "<div class='nome-avaliacao'>
+                    <span>{$avaliacao["NOME"]}</span>";
+            echo "<p class='descricao_evento'>
+                    {$avaliacao["OPINIAO"]}
+                </p>";
+            echo "<p class='descricao_evento'>
+                Nota: {$avaliacao["AVALIACAO"]}
+            </p>";
+            echo "</div>";
+            // echo "<tr>";
+            // echo "  <td> {$avaliacao["NOME"]}</td>";
+            // echo "  <td> {$avaliacao["OPINIAO"]}</td>";
+            // echo "  <td> {$avaliacao["AVALIACAO"]} </td>";
+            // echo "</tr>";
+        }
+        ?>
     <?php
     if (isset($_GET["msg"])) {
         echo $_GET["msg"];
@@ -90,18 +131,20 @@
     </div>
     <script>
         //querySelector('svg.fa-star') //trocar classe da star
-        let avaliacoes = document.querySelectorAll(".avaliacao_input");
-        avaliacoes.forEach(teste);
+        const avaliacoesInput = document.querySelectorAll(".avaliacao_input");
+        const avaliacoesLabel = document.querySelectorAll(".avaliacao_label");
+        avaliacoesInput.forEach(teste);
 
         function teste(item) {
             item.addEventListener('click', function() {
                 for (let i = 0; i < item.value; i++) {
-                    avaliacoes[i].nextElementSibling.classList.add('active');
+                    avaliacoesLabel[i].children[1].style.display = 'var(--fa-display,inline-block)';
+                    avaliacoesLabel[i].children[0].style.display = 'none';
                 }
                 for (let i = item.value; i < 5; i++) {
-                    avaliacoes[i].nextElementSibling.classList.remove('active');
+                    avaliacoesLabel[i].children[0].style.display = 'var(--fa-display,inline-block)';
+                    avaliacoesLabel[i].children[1].style.display = 'none';
                 }
-                //item.nextElementSibling.classList.toggle('active');
                 document.querySelector(".comentario").style.display = 'grid';
 
             });
